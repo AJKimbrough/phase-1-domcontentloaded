a4484aa93a39dd95548e4ffe0108fc017e26ad7e
Amber Kimbrough <akimbrough14@gmail.com>
2023-04-17 10:38:21 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', () => {
+   // document.getElementById('text').innerText = 'This is really cool!'
+   document.querySelector('p').textContent = 'This is really cool!'
+})
+
+
+
